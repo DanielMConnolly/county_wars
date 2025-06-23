f12d5c99ef18ad4babcaf267e307208187fa11db
Daniel M Connolly <danconnolly@meta.com>
2025-06-23 17:53:05 -0400

    added changes'

--- tests/CreateNewGame.test.tsx
+++ tests/CreateNewGame.test.tsx
@@ -1,33 +1,32 @@
 import puppeteer, { Browser, Page } from 'puppeteer';
-import { createNewGame , setupNewUser} from './SetupUtils';
+import { createNewGame, setupNewUser } from './SetupUtils';
 import { DataTestIDs } from '../src/DataTestIDs';
 
 let testPage: Page;
 let browser: Browser;
 
-
 beforeAll(async () => {
   browser = await puppeteer.launch();
   testPage = await browser.newPage();
   await setupNewUser(testPage);
-  testPage.goto('http://localhost:5173')
-}, 1000000);
+  await testPage.goto('http://localhost:5173')
+});
 
 describe("Create new game", () => {
   test("Verify that a game can be created and that it exists on the welcome screen after creation", async () => {
     await createNewGame(testPage, "County Conquest");
 
-    testPage.goto('http://localhost:5173')
+    await testPage.goto('http://localhost:5173')
     await testPage.waitForSelector(`[data-testid="${DataTestIDs.EXISTING_GAMES_LIST}"]`);
     let existingGamesText
-     = await testPage.$eval(`[data-testid="${DataTestIDs.EXISTING_GAMES_LIST}"]`, el => el.textContent);
+      = await testPage.$eval(`[data-testid="${DataTestIDs.EXISTING_GAMES_LIST}"]`, el => el.textContent);
     expect(existingGamesText).toContain("County Conquest");
     // reload page and verify that the game is still there
-    testPage.reload();
+    await testPage.reload();
     await testPage.waitForSelector(`[data-testid="${DataTestIDs.EXISTING_GAMES_LIST}"]`);
     existingGamesText
-    = await testPage.$eval(`[data-testid="${DataTestIDs.EXISTING_GAMES_LIST}"]`, el => el.textContent);
-   expect(existingGamesText).toContain("County Conquest");
+      = await testPage.$eval(`[data-testid="${DataTestIDs.EXISTING_GAMES_LIST}"]`, el => el.textContent);
+    expect(existingGamesText).toContain("County Conquest");
 
   });
 });
