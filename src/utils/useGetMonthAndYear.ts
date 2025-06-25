4ce3fe9a09fb10e2944042180a6019dfb1253683
Daniel M Connolly <danconnolly@meta.com>
2025-06-24 21:31:47 -0400

    adding limits on where franchises can be placed

--- src/utils/useGetMonthAndYear.ts
+++ src/utils/useGetMonthAndYear.ts
@@ -1,16 +1,16 @@
 import { useGameState } from "../GameStateContext";
 
 
-const useGetMonthAndYear: ()=> {
+const useGetMonthAndYear: () => {
     year: number,
     month: number,
 } = () => {
 
-    const {gameState} = useGameState();
-    const {gameTime} = gameState;
+    const { gameState } = useGameState();
+    const { gameTime } = gameState;
 
     const totalGameMs = gameTime.gameDurationHours * 60 * 60 * 1000;
-    const elapsedTime = gameTime.elapsedTime??0;
+    const elapsedTime = gameTime.elapsedTime ?? 0;
     const progress = Math.min(elapsedTime / totalGameMs, 1);
     const totalMonths = 80 * 12;
     const currentMonthIndex = Math.floor(progress * totalMonths);
